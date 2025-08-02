3f030a2ef1e06272d85779084c49d95b41893b27
iagoalvess <iagoalves.voador@gmail.com>
2025-08-02 18:33:00 -0300

    feat: Expose POST /api/races endpoint to create race events

---
+++ src/main/java/br/com/racemanager/api/controller/RaceEventController.java
@@ -0,0 +1,35 @@
+package br.com.racemanager.api.controller;
+
+import br.com.racemanager.api.dto.RaceEventRequest;
+import br.com.racemanager.api.dto.RaceEventResponse;
+import br.com.racemanager.api.service.RaceEventService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/api/races")
+@RequiredArgsConstructor
+public class RaceEventController {
+    private final RaceEventService raceEventService;
+
+    @PostMapping
+    public ResponseEntity<RaceEventResponse> create(@RequestBody @Valid RaceEventRequest request) {
+        RaceEventResponse response = raceEventService.create(request);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(response.id())
+                .toUri();
+
+        return ResponseEntity.created(location).body(response);
+    }
+}
