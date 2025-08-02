fc6e452f95da57575b8157002718fdea1a4c1025
iagoalvess <iagoalves.voador@gmail.com>
2025-08-02 19:35:59 -0300

    refactor(mapper): Add update method using @MappingTarget

--- src/main/java/br/com/racemanager/api/mapper/RaceEventMapper.java
+++ src/main/java/br/com/racemanager/api/mapper/RaceEventMapper.java
@@ -4,6 +4,7 @@ import br.com.racemanager.api.dto.RaceEventRequest;
 import br.com.racemanager.api.dto.RaceEventResponse;
 import br.com.racemanager.api.model.RaceEvent;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
 
 import java.util.List;
 
@@ -14,4 +15,6 @@ public interface RaceEventMapper {
     RaceEventResponse toResponse(RaceEvent event);
 
     List<RaceEventResponse> toResponse(List<RaceEvent> events);
+
+    void updateEntityFromRequest(RaceEventRequest request, @MappingTarget RaceEvent entity);
 }
