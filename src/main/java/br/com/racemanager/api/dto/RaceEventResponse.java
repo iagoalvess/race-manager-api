cb33ebb350f42f1a40226a74c2225ad570c38ed1
iagoalvess <iagoalves.voador@gmail.com>
2025-08-02 18:21:20 -0300

    fix: RaceEventResponse from class to record

--- src/main/java/br/com/racemanager/api/dto/RaceEventResponse.java
+++ src/main/java/br/com/racemanager/api/dto/RaceEventResponse.java
@@ -2,10 +2,11 @@ package br.com.racemanager.api.dto;
 
 import java.time.LocalDate;
 
-public class RaceEventResponse {
-    Long id;
-    String name;
-    LocalDate eventDate;
-    String location;
-    Double distanceInKm;
+public record RaceEventResponse (
+    Long id,
+    String name,
+    LocalDate eventDate,
+    String location,
+    Double distanceInKm
+) {
 }
