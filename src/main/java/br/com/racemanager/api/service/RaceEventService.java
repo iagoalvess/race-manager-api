0102e34bb3d6da53835fe5df27ab776f4236cf10
iagoalvess <iagoalves.voador@gmail.com>
2025-08-02 19:01:28 -0300

    feat: Implement service layer to list all race events

--- src/main/java/br/com/racemanager/api/service/RaceEventService.java
+++ src/main/java/br/com/racemanager/api/service/RaceEventService.java
@@ -2,15 +2,19 @@ package br.com.racemanager.api.service;
 
 import br.com.racemanager.api.dto.RaceEventRequest;
 import br.com.racemanager.api.dto.RaceEventResponse;
+import br.com.racemanager.api.mapper.RaceEventMapper;
 import br.com.racemanager.api.model.RaceEvent;
 import br.com.racemanager.api.repository.RaceEventRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class RaceEventService {
     private final RaceEventRepository raceEventRepository;
+    private final RaceEventMapper raceEventMapper;
 
     public RaceEventResponse create(RaceEventRequest request) {
         RaceEvent newRaceEvent = new RaceEvent();
@@ -28,5 +32,11 @@ public class RaceEventService {
                 savedRaceEvent.getLocation(),
                 savedRaceEvent.getDistanceInKm()
         );
+    }
+
+    public List<RaceEventResponse> findAll() {
+        List<RaceEvent> events = raceEventRepository.findAll();
+
+        return raceEventMapper.toResponse(events);
     }
 }
