cde0f3951941d3fff980d3cf2116a3755a37423e
iagoalvess <iagoalves.voador@gmail.com>
2025-08-02 17:23:53 -0300

    Initial commit: Project generated with Spring Initializer

---
+++ src/main/java/br/com/racemanager/api/RaceManagementApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.racemanager.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaceManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaceManagementApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/racemanager/api/RaceManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.racemanager.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaceManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
