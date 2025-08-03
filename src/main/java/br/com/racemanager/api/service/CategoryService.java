8098e8ac67f6dd66e2b6eb1f2a1ce16b00a378ae
iagoalvess <iagoalves.voador@gmail.com>
2025-08-03 09:56:52 -0300

    feat: Implement service layer to create a category for a race event

---
+++ src/main/java/br/com/racemanager/api/service/CategoryService.java
@@ -0,0 +1,31 @@
+package br.com.racemanager.api.service;
+
+import br.com.racemanager.api.dto.CategoryRequest;
+import br.com.racemanager.api.dto.CategoryResponse;
+import br.com.racemanager.api.exception.ResourceNotFoundException;
+import br.com.racemanager.api.mapper.CategoryMapper;
+import br.com.racemanager.api.model.Category;
+import br.com.racemanager.api.model.RaceEvent;
+import br.com.racemanager.api.repository.CategoryRepository;
+import br.com.racemanager.api.repository.RaceEventRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CategoryService {
+    private final CategoryRepository categoryRepository;
+    private final RaceEventRepository raceEventRepository;
+    private final CategoryMapper categoryMapper;
+
+    public CategoryResponse create(Long raceId, CategoryRequest request) {
+        RaceEvent raceEvent = raceEventRepository.findById(raceId)
+                .orElseThrow(() -> new ResourceNotFoundException("Race not found for id: " + raceId));
+
+        Category newCategory = categoryMapper.toEntity(request);
+        newCategory.setRaceEvent(raceEvent);
+
+        Category savedCategory = categoryRepository.save(newCategory);
+        return categoryMapper.toResponse(savedCategory);
+    }
+}
