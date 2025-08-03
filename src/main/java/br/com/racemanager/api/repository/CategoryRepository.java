fe7f7ebdae7ee78b6a2fcbdf8d04074937e02f80
iagoalvess <iagoalves.voador@gmail.com>
2025-08-03 11:30:02 -0300

    feat: Implement business logic to register a participant

--- src/main/java/br/com/racemanager/api/repository/CategoryRepository.java
+++ src/main/java/br/com/racemanager/api/repository/CategoryRepository.java
@@ -3,11 +3,16 @@ package br.com.racemanager.api.repository;
 import br.com.racemanager.api.model.Category;
 import br.com.racemanager.api.model.RaceEvent;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
     List<Category> findByRaceEventId(Long raceId);
+
+    @Query("SELECT c FROM Category c WHERE c.raceEvent.id = :raceId AND c.gender = :gender AND :age BETWEEN c.minAge AND c.maxAge")
+    Optional<Category> findCategoryForParticipant(Long raceId, int age, String gender);
 }
--- src/main/java/br/com/racemanager/api/repository/ParticipantRepository.java
+++ src/main/java/br/com/racemanager/api/repository/ParticipantRepository.java
@@ -4,6 +4,9 @@ import br.com.racemanager.api.model.Participant;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ParticipantRepository extends JpaRepository<Participant, Long> {
+    Optional<Participant> findTopByRaceEventIdOrderByBibNumberDesc(Long raceEventId);
 }
